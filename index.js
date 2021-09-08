51e707f43b62585131cd8202ea225435dbc3bf3a
leonpiedrahita <leondpr@gmail.com>
2021-09-07 21:37:02 -0500

    cambio

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));//esto es para leer este tipo
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://quiet-plains-55064.herokuapp.com/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
